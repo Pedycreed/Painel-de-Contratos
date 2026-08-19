1e88f432f0e280ea19d2c86a694001413e081145
Jhonatan _Salgueiro <99848013+Pedycreed@users.noreply.github.com>
2026-08-19 15:38:27 -0300

    Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import react from "@vitejs/plugin-react";
 // editar isso manualmente com o nome do seu repositório.
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: '/Painel-de-Contratos/',
 });
